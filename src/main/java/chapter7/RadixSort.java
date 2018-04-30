f259f64075334f21ac80e5db7a42d2db375bf309
pozitron <pmpozitron@gmail.com>
2018-04-30 09:48:06 +0300

    7.5 RadixSort processes numbers up to Integer.MAX_VALUE

--- src/main/java/chapter7/RadixSort.java
+++ src/main/java/chapter7/RadixSort.java
@@ -26,16 +26,24 @@ public class RadixSort {
 
     private static int[] randomValues(int size) {
         int[]result = new int[size];
-        for (int i=0; i<size; i++){
-            result[i] = (int)(Math.random() * 1000);
+        result[0] = Integer.MAX_VALUE;
+
+        for (int i=1; i<size-1; i++){
+            if (i == size/2) {
+                result[i] = Integer.MAX_VALUE;
+            } else {
+                result[i] = (int) (Math.random() * Integer.MAX_VALUE);
+            }
         }
+        result[size-1] = Integer.MAX_VALUE;
 
         return result;
     }
 
     public static void main(String[] args) {
         RadixSort sort  = new RadixSort();
-        for (int i = 1; i < 1000; i*=10) {
+        for (int i = 1; i > 0 && i < Integer.MAX_VALUE; i*=10) {
+//        for (int i = 1; i < 214748364; i*=10) {
             for (int value : sort.getValues()) {
                 int digit = value / i % 10;
                 System.out.println(digit);
